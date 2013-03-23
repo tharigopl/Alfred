60d36decfff466e721ebf3a6da27357b9d87f31d
Jonathan Keith <jk@jonkeith.com>
2013-03-23 11:30:07 -0500

    random IEnumerable selector extension

---
+++ Bot/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static T Random<T> (this IEnumerable<T> items) {
+            var count = items.Count();
+            if (count == 0) return default(T);
+
+            var r = new Random(DateTime.Now.Millisecond);
+            var index = r.Next(1, count);
+
+            return items.ElementAt(index);
+        }
+    }
+}
