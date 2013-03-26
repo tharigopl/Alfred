09487922b0b127f789180a973b31138fc50a2418
Jonathan Keith <jk@jonkeith.com>
2013-03-26 17:42:51 -0500

    give admins a voice

--- Bot/Commands/SayCommand.cs
+++ Bot/Commands/SayCommand.cs
@@ -12,11 +12,25 @@ namespace Bot.Commands
     {
         public override void Process(IrcCommand command)
         {
+            base.Process(command);
+
             if (!HasAdminUser())
                 return;
 
-            if (HandleNoParameters("You have to tell me what to say."))
+            if (command.Parameters.Length < 2)
+            {
+                SendMessage(
+                    "You have to tell me what to say and where to say it: say <channel | user> message"
+                );
                 return;
+            }
+
+            var target = command.Parameters[0];
+            var message = string.Join(" ", command.Parameters, 1, command.Parameters.Length - 1);
+            command.Client.LocalUser.SendMessage(
+                target,
+                message
+            );
         }
     }
 }
