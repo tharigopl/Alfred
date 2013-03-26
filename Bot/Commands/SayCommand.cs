98e54c126428dfb15e57ac28f42c006b34bf5e4f
Jonathan Keith <jk@jonkeith.com>
2013-03-26 17:12:20 -0500

    initial say command

---
+++ Bot/Commands/SayCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Commands.Attributes;
+
+namespace Bot.Commands
+{
+    [IrcCommand("say")]
+    public class SayCommand : IrcCommandProcessor
+    {
+        public override void Process(IrcCommand command)
+        {
+            if (!HasAdminUser())
+                return;
+
+            if (HandleNoParameters("You have to tell me what to say."))
+                return;
+        }
+    }
+}
