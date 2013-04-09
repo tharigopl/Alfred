06ab63a63995dc719f51184eab505eda6b1a09d0
joncse <jk@jonkeith.com>
2013-04-09 08:33:03 -0500

    null check parameter extension

---
+++ Bot/Extensions/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static void Required(this object o, string paramName)
+        {
+            if (o == null || (o is string && string.IsNullOrEmpty(o as string)))
+                throw new ArgumentException("Required parameter missing.", paramName);
+        }
+    }
+}
